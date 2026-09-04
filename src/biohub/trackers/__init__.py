8e54f4e0ee7dec7d6a03faeccb78cd0bd2f859d8
Daniel Ryu <yjryu2424@gmail.com>
2026-09-04 14:51:34 -0400

    feat: export audited HOCT checkpoint registry

--- src/biohub/trackers/__init__.py
+++ src/biohub/trackers/__init__.py
@@ -6,10 +6,22 @@ from .hoct_compat import (
     HOCTPointGraphConfig,
     build_hoct_point_graph,
 )
+from .hoct_models import (
+    HOCT_MODELS,
+    HOCTCheckpointError,
+    HOCTModelSpec,
+    checkpoint_sha256,
+    verify_hoct_checkpoint,
+)
 
 __all__ = [
+    "HOCT_MODELS",
     "HOCT_POINT_API_IMPLEMENTED",
     "HOCT_REVISION",
+    "HOCTCheckpointError",
+    "HOCTModelSpec",
     "HOCTPointGraphConfig",
     "build_hoct_point_graph",
+    "checkpoint_sha256",
+    "verify_hoct_checkpoint",
 ]
